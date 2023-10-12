fab0752669df13ec1afcd836e3adc979757f9219
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2023-10-12 14:20:47 +0100

    Moved auth.authenticationMiddleware(tokenVerifier, smokeTestStore) back into setUpCurrentUser.ts as suggested in code review. Also moved create redisClient into dataAccess() as suggested.

--- server/middleware/setUpCurrentUser.ts
+++ server/middleware/setUpCurrentUser.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
+import auth from '../authentication/auth'
+import tokenVerifier from '../data/tokenVerification'
 import populateCurrentUser from './populateCurrentUser'
 import type { Services } from '../services'
 import populateCurrentUserRoles from './populateCurrentUserRoles'
 
-export default function setUpCurrentUser({ userService }: Services): Router {
+export default function setUpCurrentUser({ userService, smokeTestStore }: Services): Router {
   const router = Router({ mergeParams: true })
+  router.use(auth.authenticationMiddleware(tokenVerifier, smokeTestStore))
   router.use(populateCurrentUser(userService))
   router.use(populateCurrentUserRoles())
   return router
