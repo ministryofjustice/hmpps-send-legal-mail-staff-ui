28a4301bf8f02430b65c622622c5730f377272da
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2025-03-11 09:56:10 +0000

    Fix dependencies, upgrade eslint (#129)

--- server/services/dpsComponentService.ts
+++ server/services/dpsComponentService.ts
@@ -1,7 +1,6 @@
 import DpsComponentsClient, { AvailableComponent, Component } from '../data/dpsComponentClient'
 
 export default class DpsComponentService {
-  // eslint-disable-next-line
   constructor(private readonly dpsFeComponentsClient: DpsComponentsClient) {}
 
   public async getComponent(component: AvailableComponent, token: string): Promise<Component> {
